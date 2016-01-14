7474e57b9a2390dc3a4f0c6db43fa83e3b7c059c
Jack <jackov@126.com>
2016-01-14 20:02:26 +0800

    中断

--- src/app/directive/alte/alte.directive.module.js
+++ src/app/directive/alte/alte.directive.module.js
@@ -9,6 +9,7 @@ angular
   .module('water.directive.alte', [])
   .directive('alteLayout', alte.WrapperDriective)
   .directive('alteHeader', alte.HeaderDriective)
+  .directive('alteTooggleBtn', alte.toggleBtnDriective)
   .directive('alteSidebar', alte.SidebarDriective)
   .directive('alteFooter', alte.FooterDriective);
 
--- src/app/directive/alte/alte.layout.directive.js
+++ src/app/directive/alte/alte.layout.directive.js
@@ -71,6 +71,36 @@ export function HeaderDriective() {
   };
   return directive;
 }
+// 切换按钮声明, 切换侧边栏按钮
+export function toggleBtnDriective() {
+  let directive = {
+    link: function(scope, element) {
+      element.on('click', function() {
+        
+      });
+    }
+  };
+
+  return directive;
+}
+
+
+// //Enable sidebar push menu
+// if ($(window).width() > (screenSizes.sm - 1)) {
+//   if ($("body").hasClass('sidebar-collapse')) {
+//     $("body").removeClass('sidebar-collapse').trigger('expanded.pushMenu');
+//   } else {
+//     $("body").addClass('sidebar-collapse').trigger('collapsed.pushMenu');
+//   }
+// }
+// //Handle sidebar push menu for small screens
+// else {
+//   if ($("body").hasClass('sidebar-open')) {
+//     $("body").removeClass('sidebar-open').removeClass('sidebar-collapse').trigger('collapsed.pushMenu');
+//   } else {
+//     $("body").addClass('sidebar-open').trigger('expanded.pushMenu');
+//   }
+// }
 
 // 布局侧边栏
 export function SidebarDriective() {
